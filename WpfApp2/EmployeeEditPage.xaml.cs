2032b20a300495fd3fa20afd61117256ee6381de
bogdan01official <108180181+bogdan01official@users.noreply.github.com>
2024-11-22 14:31:57 +0500

    Add files via upload

---
+++ WpfApp2/EmployeeEditPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using WpfApp2;
+
+namespace EmployeeManagement
+{
+    public partial class EmployeeEditPage : Page
+    {
+        public event Action<Employee> EmployeeUpdated;
+
+        private Employee _employee;
+
+        public EmployeeEditPage(Employee employee)
+        {
+            InitializeComponent();
+            _employee = employee;
+            NameTextBox.Text = _employee.Name;
+            PositionTextBox.Text = _employee.Position;
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            _employee.Name = NameTextBox.Text;
+            _employee.Position = PositionTextBox.Text;
+
+            EmployeeUpdated?.Invoke(_employee);
+            NavigationService.GoBack();
+        }
+    }
+}
---
+++ WpfApp2/EmployeeListPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Controls;
+using System.Windows;
+using WpfApp2;
+
+namespace EmployeeManagement
+{
+    public partial class EmployeeListPage : Page
+    {
+        public ObservableCollection<Employee> Employees { get; set; }
+
+        public EmployeeListPage()
+        {
+            InitializeComponent();
+            Employees = new ObservableCollection<Employee>
+            {
+                new Employee { Name = "Сидоров Сидор Сидорович", Position = "Торговец" },
+                new Employee { Name = "Дурков Павел Андреевич", Position = "Разработчик" },
+                new Employee { Name = "Иванов Илья Сергеевич", Position = "Топ-менеджер" }
+            };
+            EmployeeDataGrid.ItemsSource = Employees;
+        }
+
+        private void EditButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (EmployeeDataGrid.SelectedItem is Employee selectedEmployee)
+            {
+                var editPage = new EmployeeEditPage(selectedEmployee);
+                editPage.EmployeeUpdated += OnEmployeeUpdated;
+                NavigationService.Navigate(editPage);
+            }
+            else
+            {
+                MessageBox.Show("Пожалуйста, выберите сотрудника для редактирования.");
+            }
+        }
+
+        private void OnEmployeeUpdated(Employee updatedEmployee)
+        {   
+            var employee = Employees[Employees.IndexOf(updatedEmployee)];
+            employee.Name = updatedEmployee.Name;
+            employee.Position = updatedEmployee.Position;
+
+            EmployeeDataGrid.Items.Refresh();
+        }
+    }
+}
